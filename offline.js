78e59d6158dca9fb4fc05c04f30182fbc335a6c7
Hoang Anh Dinh Ngoc <24842197+hoanganhdinhngoc@users.noreply.github.com>
2018-08-03 21:17:09 +0700

    Test upload Nier Automata Shoting Game
    The first test, website still not working btw but not a big deal

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1533303909,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground2.png",
+		"images/shooter-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/coretarget-sheet0.png",
+		"images/redbullet-sheet0.png",
+		"images/violetbullet-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/coretargetld-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
