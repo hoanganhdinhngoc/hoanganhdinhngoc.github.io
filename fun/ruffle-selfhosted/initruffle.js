00523f759ba20c48f93a71fba3aa26ea2ad50a68
Hoang Anh Dinh Ngoc <24842197+hoanganhdinhngoc@users.noreply.github.com>
2024-08-02 04:34:54 +0700

    Update initruffle.js

--- fun/ruffle-selfhosted/initruffle.js
+++ fun/ruffle-selfhosted/initruffle.js
@@ -1,88 +1,45 @@
+function embedSWF(url, cont, width, height) {
+    var ruffle = window.RufflePlayer.newest(),
+        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
+            width: width,
+            height: height,
+            style: 'width: ' + width + 'px; height: ' + height + 'px',
+        });
 
-// Frank's Adventure Series - ID: franksAdventure1:
-
-function franks_adventure_1() {
-    var swfobject = {};
-
-    swfobject.embedSWF = function (url, cont, width, height) {
-        var ruffle = window.RufflePlayer.newest(),
-            player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-                width: width,
-                height: height,
-                style: 'width: ' + width + 'px; height: ' + height + 'px',
-            });
-
-        player.load({ url: url });
-    };
-
-    swfobject.embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/franks-adventure-1/franks-adventure-1.swf', 'ruffle', 1500, 800);
+    player.load({ url: url });
 }
 
-
+// Frank's Adventure Series - ID: franks-adventure-1
+function franks_adventure_1() {
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/franks-adventure-1/franks-adventure-1.swf', 'ruffle', 1500, 800);
+}
 
 function franks_adventure_2() {
-    var swfobject = {};
-
-    swfobject.embedSWF = function (url, cont, width, height) {
-        var ruffle = window.RufflePlayer.newest(),
-            player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-                width: width,
-                height: height,
-                style: 'width: ' + width + 'px; height: ' + height + 'px',
-            });
-
-        player.load({ url: url });
-    };
-
-    swfobject.embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/franks-adventure-2/franks-adventure-2.swf', 'ruffle', 1500, 800);
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/franks-adventure-2/franks-adventure-2.swf', 'ruffle', 1500, 800);
 }
-
-
 
 function franks_adventure_3() {
-    var swfobject = {};
-
-    swfobject.embedSWF = function (url, cont, width, height) {
-        var ruffle = window.RufflePlayer.newest(),
-            player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-                width: width,
-                height: height,
-                style: 'width: ' + width + 'px; height: ' + height + 'px',
-            });
-
-        player.load({ url: url });
-    };
-
-    swfobject.embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/franks-adventure-3/franks-adventure-3.swf', 'ruffle', 1500, 800);
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/franks-adventure-3/franks-adventure-3.swf', 'ruffle', 1500, 800);
 }
-
-
 
 function franks_adventure_4() {
-    var swfobject = {};
-
-    swfobject.embedSWF = function (url, cont, width, height) {
-        var ruffle = window.RufflePlayer.newest(),
-            player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-                width: width,
-                height: height,
-                style: 'width: ' + width + 'px; height: ' + height + 'px',
-            });
-
-        player.load({ url: url });
-    };
-
-    swfobject.embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/franks-adventure-4/franks-adventure-4.swf', 'ruffle', 1500, 800);
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/franks-adventure-4/franks-adventure-4.swf', 'ruffle', 1500, 800);
 }
 
+// Simgirls Series
+function simgirls_5_5() {
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/imgirls-5.5/simgirls-5.5.swf', 'ruffle', 1500, 800);
+}
 
+function simgirls_6_6() {
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/imgirls-6.6/simgirls-6.6.swf', 'ruffle', 1500, 800);
+}
 
-
-
-
+function simgirls_7_7() {
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/imgirls-7.7/simgirls-7.7.swf', 'ruffle', 1500, 800);
+}
 
 // initruffle to call the correct function to load the right swf
-
 function initruffle() {
     var gameElement = document.querySelector('.gamearea');
     if (gameElement) {
