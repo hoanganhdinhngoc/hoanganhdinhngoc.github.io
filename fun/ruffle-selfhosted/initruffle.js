9c10c5fde8161c1ec98e83854c71c0ee2b34e6d4
Hoang Anh Dinh Ngoc <24842197+hoanganhdinhngoc@users.noreply.github.com>
2024-08-02 05:36:51 +0700

    update

--- fun/ruffle-selfhosted/initruffle.js
+++ fun/ruffle-selfhosted/initruffle.js
@@ -9,6 +9,13 @@ function embedSWF(url, cont, width, height) {
     player.load({ url: url });
 }
 
+// Visual Novel
+function how_dark_priestess_uriki_got_her_groove_back_by_purplemantis() {
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/how-dark-priestess-uriki-got-her-groove-back-by-purplemantis/how-dark-priestess-uriki-got-her-groove-back-by-purplemantis.swf', 'ruffle', 1500, 800);
+}
+
+
+
 // Frank's Adventure Series - ID: franks-adventure-1
 function franks_adventure_1() {
     embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/franks-adventure-1/franks-adventure-1.swf', 'ruffle', 1500, 800);
@@ -28,15 +35,23 @@ function franks_adventure_4() {
 
 // Simgirls Series
 function simgirls_5_5() {
-    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/imgirls-5.5/simgirls-5.5.swf', 'ruffle', 1500, 800);
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/simgirls-5.5/simgirls-5.5.swf', 'ruffle', 1500, 800);
 }
 
 function simgirls_6_6() {
-    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/imgirls-6.6/simgirls-6.6.swf', 'ruffle', 1500, 800);
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/simgirls-6.6/simgirls-6.6.swf', 'ruffle', 1500, 800);
 }
 
 function simgirls_7_7() {
-    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/imgirls-7.7/simgirls-7.7.swf', 'ruffle', 1500, 800);
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/simgirls-7.7/simgirls-7.7.swf', 'ruffle', 1500, 800);
+}
+
+function simgirls_rosebery_teaser() {
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/simgirls-rosebery-teaser/simgirls-rosebery-teaser.swf', 'ruffle', 1500, 800);
+}
+
+function simgirls_tomokos_story() {
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/simgirls-tomokos-story/706364_simgirls-vn.swf', 'ruffle', 1500, 800);
 }
 
 // initruffle to call the correct function to load the right swf
