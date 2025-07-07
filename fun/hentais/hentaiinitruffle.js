c02c9343cc752299b981b93faac25473de61d943
Hoang Anh Dinh Ngoc <24842197+hoanganhdinhngoc@users.noreply.github.com>
2025-07-07 18:09:06 +0700

    Update hentaiinitruffle.js

--- fun/hentais/hentaiinitruffle.js
+++ fun/hentais/hentaiinitruffle.js
@@ -111,7 +111,7 @@ function meet_and_fuck_jessica_fuck_machine() {
 }
 
 function meet_and_fuck_dance_school() {
-    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/archive/meet-and-fuck-dance-school.swf', 'ruffle', 1500, 1100);
+    embedSWF('https://hoanganhdinhngoc.github.io/fun/hentais/archive/meet-and-fuck-dance-school.swf', 'ruffle', 1200, 1000);
 }
 
 function meet_and_fuck_denise_milani() {
