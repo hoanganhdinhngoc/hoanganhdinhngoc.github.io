2e49bf74bc7f81a1a3dedd4eb84edd5736c56e70
Hoang Anh Dinh Ngoc <24842197+hoanganhdinhngoc@users.noreply.github.com>
2025-11-21 02:47:48 +0700

    add fun-additional-script.js to "the-blade-of-innocence" html file

--- fun/js/fun-additional-script.js
+++ fun/js/fun-additional-script.js
@@ -1,1 +1,7 @@
-$.getScript( "Additional code" )
+
+window.RufflePlayer = window.RufflePlayer || {};
+window.RufflePlayer.config = {
+// Other configurations can go here
+"publicPath": undefined, // use undefined for default
+"openUrlMode": "block" // This blocks all external links and network requests
+};
